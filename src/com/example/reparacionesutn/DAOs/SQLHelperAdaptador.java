bf7fd19485a6cf9840a51bd72d358d83fd6acbd1
diegotestereo <diegotestereo@gmail.com>
2014-06-24 11:26:27 -0300

    etb

---
+++ src/com/example/reparacionesutn/DAOs/SQLHelperAdaptador.java
@@ -0,0 +1,27 @@
+package com.example.reparacionesutn.DAOs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLHelperAdaptador extends SQLiteOpenHelper {
+
+	public SQLHelperAdaptador(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+		
+	}
+
+}
