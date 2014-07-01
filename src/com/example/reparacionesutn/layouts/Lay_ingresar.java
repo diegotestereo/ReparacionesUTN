2651f0ece8c391e02d02c13a6bc077fd30cffc9b
diegotestereo <diegotestereo@gmail.com>
2014-07-01 16:07:58 -0300

    etb
    ii

--- src/com/example/reparacionesutn/layouts/Lay_ingresar.java
+++ src/com/example/reparacionesutn/layouts/Lay_ingresar.java
@@ -1,6 +1,10 @@
 package com.example.reparacionesutn.layouts;
 
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import com.example.reparacionesutn.R;
 import com.example.reparacionesutn.DAOs.SQLHelperAdaptador;
 
@@ -13,25 +17,29 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Lay_ingresar extends Activity{
-	
+	TextView txt_date;
 	Spinner spin_modelos,spin_fallas,spin_versiones;
 	EditText etxt_serial,observaciones;
 	Button btn_IngresarReparacion;
 	ArrayAdapter<String> adaptadorModelos,adaptadorVersiones,adaptadorFallas;
 	SQLHelperAdaptador dao;
-	
-	
+	java.util.Date date = new Date();
+	java.text.SimpleDateFormat sdf=new java.text.SimpleDateFormat("dd/MM/yyyy");
+	String fecha = sdf.format(date);
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lay_ingresar);
 		levantarXML();
+		
 		Botones();
 		cargaAdaptadores();
+		txt_date.setText(fecha);
 	}
 
 	private void cargaAdaptadores() {
@@ -53,18 +61,23 @@ public class Lay_ingresar extends Activity{
 			
 			@Override
 			public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "Reparacion Serial:", Toast.LENGTH_SHORT).show();finish();
-				
+			Toast.makeText(getApplicationContext(), "Reparacion Serial:" + etxt_serial.getText().toString(), Toast.LENGTH_SHORT).show();finish();
+		
 			}
 		});
 	}
 
 	private void levantarXML() {
+		
 		spin_modelos=(Spinner) findViewById(R.id.spin_modelos);
 		spin_fallas=(Spinner) findViewById(R.id.spin_fallas);
 		spin_versiones=(Spinner) findViewById(R.id.spin_versiones);
+		
 		etxt_serial=(EditText) findViewById(R.id.eTxt_Serial_ingresar);
 		observaciones=(EditText) findViewById(R.id.etxt_observaciones);
+		
+		txt_date=(TextView) findViewById(R.id.txtV_Date);
+			
 		btn_IngresarReparacion = (Button) findViewById(R.id.btn_IngresarReparacion);
 	}
 
